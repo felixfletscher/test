51d5dcc450982ff6480fdb26d931d1eb762fe0d2
Meier221 <felixmeier.mailing@gmail.com>
2021-11-30 12:53:08 +0100

    upload module

---
+++ webdesign/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': 'Webdesign',
+    'description': 'Website Snippets Felix.',
+    'version': '14.0',
+    'author': 'Felix Meier, felix@flfetscher.de',
+    'category': 'Theme/Creative',
+
+    'depends': ['website'],
+    #menu must be last in data!
+    'data': [
+        'views/pages.xml',
+        #'views/assets.xml',
+        'views/layout.xml',
+        'views/snippet1.xml',
+        'views/snippet2.xml',
+        'views/snippet3.xml',
+        'views/snippet4.xml',
+        'views/snippet5.xml',
+        'views/snippet6.xml',
+        'views/snippet7.xml',
+        'views/snippet8.xml',
+        'views/snippet9.xml',
+        'views/menu.xml'
+    ],
+
+}
+
